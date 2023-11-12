f94c2528a0a127d19eb7261750aef1dc48d5ed9b
Lucas Alves <as.lucasalves@gmail.com>
2023-11-12 18:09:53 -0300

    feat: create form imc

--- app/src/main/java/com/lucasadasial/simplefittracker/ImcActivity.kt
+++ app/src/main/java/com/lucasadasial/simplefittracker/ImcActivity.kt
@@ -2,10 +2,20 @@ package com.lucasadasial.simplefittracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class ImcActivity : AppCompatActivity() {
+
+    private lateinit var  editWeight: EditText
+    private lateinit var  editHeight: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
+
+        editWeight = findViewById(R.id.editTextWeight)
+        editHeight = findViewById(R.id.editTextHeight)
+
+        val btnCalculate = findViewById<Button>(R.id.btn_calculate)
     }
 }
